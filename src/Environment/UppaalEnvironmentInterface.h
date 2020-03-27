12e4717a31e9e1bebd2104cbebddba036df62a91
amijacika <adrianamijacika@gmail.com>
2020-03-27 12:34:18 +0100

    Uppaal Interface created

---
+++ src/Environment/UppaalEnvironmentInterface.h
@@ -0,0 +1,16 @@
+#ifndef UPPAALENVIRONMNENT_H
+#define UPPAALENVIRONMENT_H
+
+#include <EnvironmentInterface.h>
+
+class UppaalEnvironmentInterface : public EnvironmentInterface {
+
+  public:
+    virtual std::tuple<int, int> GetDelayBounds(State &state) = 0;
+
+    virtual std::tuple<State, bool> DelayState(State &state, int delay) = 0;
+
+    virtual std::vector<State> GetValidChildStatesNoDelay(State &state) = 0;
+};
+
+#endif // UPPAALENVIRONMENTINTERFACE_H
