2d46c51b4794b44dcd993b55ba4b8e9f8e026467
amijacika <adrianamijacika@gmail.com>
2020-04-28 09:48:49 +0200

    fixed stuff that prevented finding terminal node for 4 people

--- src/Core/UCT_PTA.cpp
+++ src/Core/UCT_PTA.cpp
@@ -2,19 +2,21 @@
 // Created by happysun on 27/12/2019.
 //
 
-#include <cfloat>
-#include <cassert>
-#include <cmath>
 #include <UCT_PTA.h>
+#include <cassert>
+#include <cfloat>
+#include <cmath>
 
 UCT_PTA::UCT_PTA(EnvironmentInterface &environment)
-: _environment(environment), generator(std::mt19937(time(nullptr))), _defaultPolicy(UPPAAL_RandomSamplingDefaultPolicy(_environment)), root_node(SearchNode::create_SearchNode(nullptr, false)) {
+    : _environment(environment), generator(std::mt19937(time(nullptr))),
+      _defaultPolicy(UPPAAL_RandomSamplingDefaultPolicy(_environment)),
+      root_node(SearchNode::create_SearchNode(nullptr, false)) {
     // UCT TreePolicy setup
     std::function<std::shared_ptr<SearchNode>(std::shared_ptr<SearchNode>)> f_expand =
-            std::bind(&UCT_PTA::m_expand, this, std::placeholders::_1);
+        std::bind(&UCT_PTA::m_expand, this, std::placeholders::_1);
 
     std::function<std::shared_ptr<SearchNode>(std::shared_ptr<SearchNode>, double)> f_best_child =
-            std::bind(&UCT_PTA::m_best_child, this, std::placeholders::_1, std::placeholders::_2);
+        std::bind(&UCT_PTA::m_best_child, this, std::placeholders::_1, std::placeholders::_2);
 
     _tpolicy = UCT_TreePolicy(f_expand, f_best_child);
 }
@@ -31,7 +33,7 @@ State UCT_PTA::run(int n_searches) {
     m_root->set_unvisited_child_states(unvisited_child_states);
 
     // rough bootstrap of reward scaling
-    std::vector<double> rewards(100, 0);
+    std::vector<double> rewards(500, 0);
     for (int i = 0; i < 100; ++i) {
         Reward score = m_default_policy(m_root->state);
         rewards.at(i) = score;
@@ -41,35 +43,27 @@ State UCT_PTA::run(int n_searches) {
     rewardMinMax.first = *it.first;
     rewardMinMax.second = *it.second;
 
-
-    while(!best_proved && max_timeLeft > 0) {
+    while (!best_proved && max_timeLeft > 0) {
         // TreePolicy runs to find an unexpanded node to expand
         auto expandedNode = m_tree_policy(m_root);
         // From the expanded node, a simulation runs that returns a score
-        std::vector<double> sim_scores(20, 0);
-        for (int i=0; i < 20; ++i){
-            Reward simulation_score = m_default_policy(expandedNode->state);
-            sim_scores.at(i) = simulation_score;
-        }
-        auto simMinMax = std::minmax_element(std::begin(sim_scores), std::end(sim_scores));
+        Reward simulation_score = m_default_policy(expandedNode->state);
         // eventually update min max reward
-        if(*simMinMax.first < rewardMinMax.first){
-            rewardMinMax.first = *simMinMax.first;
-        } else if(*simMinMax.second > rewardMinMax.second){
-            rewardMinMax.second = *simMinMax.second;
+        if (simulation_score < rewardMinMax.first) {
+            rewardMinMax.first = simulation_score;
+        } else if (simulation_score > rewardMinMax.second) {
+            rewardMinMax.second = simulation_score;
         }
-
-        auto avg_score = std::accumulate(std::begin(sim_scores), std::begin(sim_scores), 0) / sim_scores.size();
-
         // normalize data
-        double norm_score = (avg_score-rewardMinMax.first)/(rewardMinMax.second - rewardMinMax.first);
+        double norm_score = (simulation_score - rewardMinMax.first) / (rewardMinMax.second - rewardMinMax.first);
+
         // The score is backpropagated up through the search tree
         m_backpropagation(expandedNode, norm_score);
 
         // if new terminal node is encountered, update the bestTerminalNode
-        if (expandedNode->isTerminalState){
+        if (expandedNode->isTerminalState) {
             Reward termReward = _environment.EvaluateRewardFunction(expandedNode->state);
-            if (bestTerminalNodesFound.empty() || bestTerminalNodesFound.back().score < termReward){
+            if (bestTerminalNodesFound.empty() || bestTerminalNodesFound.back().score < termReward) {
                 auto newBestNode = TerminalNodeScore();
                 newBestNode.score = termReward;
                 newBestNode.node = expandedNode;
@@ -86,13 +80,12 @@ State UCT_PTA::run(int n_searches) {
 
     root_node = m_root;
 
-    if (bestTerminalNodesFound.empty()){
+    if (bestTerminalNodesFound.empty()) {
         return nullptr;
     } else {
         return bestTerminalNodesFound.at(0).node->state;
     }
 }
-
 
 std::shared_ptr<SearchNode> UCT_PTA::m_best_child(std::shared_ptr<SearchNode> node, double c) {
 
@@ -123,7 +116,6 @@ std::shared_ptr<SearchNode> UCT_PTA::m_best_child(std::shared_ptr<SearchNode> no
     int i_random = uniformIntDistribution(generator);
 
     return node->child_nodes.at(bestChildren.at(i_random));
-
 }
 
 std::shared_ptr<SearchNode> UCT_PTA::m_expand(std::shared_ptr<SearchNode> node) {
@@ -152,18 +144,12 @@ std::shared_ptr<SearchNode> UCT_PTA::m_expand(std::shared_ptr<SearchNode> node) 
     return expanded_node;
 }
 
-Reward UCT_PTA::m_default_policy(State &state) {
-    return _defaultPolicy.defaultPolicy(state);
-}
+Reward UCT_PTA::m_default_policy(State &state) { return _defaultPolicy.defaultPolicy(state); }
 
-void UCT_PTA::m_backpropagation(std::shared_ptr<SearchNode> node, Reward score) {
-    return _backup.backup(node, score);
-}
+void UCT_PTA::m_backpropagation(std::shared_ptr<SearchNode> node, Reward score) { return _backup.backup(node, score); }
 
 std::shared_ptr<SearchNode> UCT_PTA::m_tree_policy(std::shared_ptr<SearchNode> node) {
     return _tpolicy.treePolicy(node);
 }
 
-std::shared_ptr<SearchNode> UCT_PTA::m_search(int n_searches) {
-    return std::shared_ptr<SearchNode>();
-}
+std::shared_ptr<SearchNode> UCT_PTA::m_search(int n_searches) { return std::shared_ptr<SearchNode>(); }
